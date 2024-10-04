b42a2df5c1eb67de5c683dd121d6a8e6adc3ce03
tup0g <mhskrip@gmail.com>
2024-10-04 19:20:34 +0300

    bagatopotochnist_task

--- virtual_methods.cpp
+++ virtual_methods.cpp
@@ -1,59 +1,70 @@
 ﻿#include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
+#include <fstream>
 
 using namespace std;
 
 class Weapon {
-public:
-    virtual void hit_damage() = 0;
-};
-
-class Bazuka : public Weapon {
-public:
-    void hit_damage() override{
-        cout << "BAM-BAM" << endl;
-    }
-};
-
-class Rifle : public Weapon {
-public:
-    void hit_damage() override {
-        cout << "ratatatatatatata" << endl;
-    }
-};
-
-class Pistol : public Weapon {
-    void hit_damage() override {
-        cout << "piw piw paw" << endl;
-    }
-};
-
-class Sniper_rifle : public Weapon {
-    void hit_damage() override {
-        cout << "BUH" << endl;
-    }
-};
-
-
-
-class Player {
-public: 
-    void Shoot(Weapon *weapon) {
-        weapon->hit_damage();
-    }
+    private:
+        int damage;
+        int ammo;
+        string naming;
+    public:
+        Weapon(int d, int a, string n){
+            damage = d;
+            ammo = a;
+            naming = n;
+        }
+        void print_info(){
+            cout << damage << endl;
+            cout << ammo << endl;
+            cout << naming << endl;
+        }
+        void save_info(const string &filename){
+            ofstream saveFile(filename);
+            if (saveFile.is_open())
+            {
+                saveFile << damage << endl;
+                saveFile << ammo << endl;
+                saveFile << naming << endl;
+                saveFile.close();
+                cout << "Saved to: " << filename << endl;
+            }
+            else
+            {
+                cout << "error in saving" << endl;
+            }
+        }
+        void open_info(const string &filename)
+        {
+            ifstream infoFile(filename);
+            if (infoFile.is_open())
+            {
+                getline(infoFile, naming);
+                infoFile >> damage;
+                infoFile >> ammo;
+                infoFile.close();
+                cout << "Loaded from: " << filename << endl;
+            }
+            else
+            {
+                cout << "error in opening" << endl;
+            }
+        }     
 };
 
 int main()
 {
-    Bazuka bazuka;
-    Rifle rifle;
-    Pistol pistol;
-    Sniper_rifle s_rifle;
+    Weapon weapon(50, 31, "Ak-174");
+    Weapon new_weapon(0,0,"");
 
-    Player player1;
-    player1.Shoot(&bazuka);
-    player1.Shoot(&rifle);
-    player1.Shoot(&s_rifle);
-    player1.Shoot(&pistol);
-    
+    thread saveThread(&Weapon::save_info, &weapon, "weapon_info.txt");
+    saveThread.join();
+
+    thread loadThread(&Weapon::open_info, &new_weapon, "weapon_data.txt");
+    loadThread.join();
+
+    new_weapon.print_info();
 }
